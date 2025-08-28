d579ac0a64d84ea2c5077b68f07bfe7a7f45f881
Bli_Aik <haikun2333@126.com>
2025-08-28 13:29:47 +0800

    chore(Unity): Remove GameObjectAdapter and refactor GameController to use object pooling
    Removed obsolete GameObjectAdapter class and its meta file. Updated GameController to implement object pooling for GameObject instances, improving performance by reusing GameObjects instead of creating/destroying them repeatedly. Added pool management with capacity limits and proper cleanup.

--- Runtime/GameController.cs
+++ Runtime/GameController.cs
@@ -1,19 +1,23 @@
 using Arch.Core;
 using Arch.Core.Extensions;
 using Arch.System;
+using UnityEngine;
+using UnityEngine.Pool;
 using Lunar.Adapters.Unity.Systems;
 using Lunar.Components;
-using UnityEngine;
 
 namespace Lunar.Adapters.Unity
 {
     public class GameController : MonoBehaviour
     {
+        private ObjectPool<UnityEngine.GameObject> _gameObjectPool;
         private World _mainWorld;
         private Group<float> _systems;
 
         private void Awake()
         {
+            SetUpGameObjectPool();
+            
             _mainWorld = World.Create();
 
             _systems = new Group<float>("MainGroup",
@@ -25,17 +29,53 @@ namespace Lunar.Adapters.Unity
             _systems.Initialize();
         }
 
+        private void SetUpGameObjectPool()
+        {
+            _gameObjectPool = new ObjectPool<UnityEngine.GameObject>(
+                createFunc: () => new UnityEngine.GameObject(),
+                actionOnGet: gameObjectItem => gameObjectItem.SetActive(true),
+                actionOnRelease: gameObjectItem => gameObjectItem.SetActive(false),
+                actionOnDestroy: Destroy,
+                collectionCheck: false,
+                defaultCapacity: 20,
+                maxSize: 200
+            );
+        }
+
         private void Update()
         {
             var deltaTime = Time.deltaTime;
             _systems.BeforeUpdate(in deltaTime);
             _systems.Update(in deltaTime);
-            
+
             //Test
 
             if (Input.GetKeyDown(UnityEngine.KeyCode.A))
             {
                 _mainWorld.Create(new GameObjectComponent());
+            }
+            
+            
+            if (Input.GetKeyDown(UnityEngine.KeyCode.Q))
+            {
+                RemoveObject();
+            }
+
+            return;
+
+            void RemoveObject()
+            {
+                var query = new QueryDescription().WithAll<GameObjectComponent>();
+                _mainWorld.Query(in query, (Entity entity, 
+                    ref GameObjectComponent gameObjectComponent) =>
+                {
+                    if (gameObjectComponent.GameObject != null)
+                    {
+                        _gameObjectPool.Release(gameObjectComponent.GameObject.BaseGameObject as UnityEngine.GameObject);
+                    }
+
+                    entity.Remove<GameObjectComponent>();
+                });
             }
         }
 
@@ -55,6 +95,7 @@ namespace Lunar.Adapters.Unity
         {
             _systems.Dispose();
             _mainWorld.Dispose();
+            _gameObjectPool.Clear();
         }
 
         private void SetEvents()
@@ -65,14 +106,15 @@ namespace Lunar.Adapters.Unity
                 {
                     entity.Add<PositionComponent>();
                 }
-                
+
                 if (gameObjectComponent.GameObject != null)
                 {
                     return;
                 }
-
-                var nativeGameObject = new UnityEngine.GameObject();
-                gameObjectComponent.GameObject = new GameObject(nativeGameObject);
+                var unityGameObject = _gameObjectPool.Get();
+                unityGameObject.transform.SetParent(transform);
+                
+                entity.Set(new GameObjectComponent(new GameObject(unityGameObject)));
             });
         }
     }
