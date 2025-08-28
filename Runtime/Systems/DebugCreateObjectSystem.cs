d960b703fd25e60c87850bedf97bb097ba437a7f
Bli_Aik <haikun2333@126.com>
2025-08-28 21:08:14 +0800

    feat(Unity): Add DebugCreateObjectSystem for testing entity creation

---
+++ Runtime/Systems/DebugCreateObjectSystem.cs
@@ -0,0 +1,40 @@
+using Arch.Core;
+using Arch.System;
+using Lunar.Components;
+using UnityEngine;
+
+namespace Lunar.Adapters.Unity.Systems
+{
+    public class DebugCreateObjectSystem : BaseSystem<World, float>
+    {
+        public DebugCreateObjectSystem(World world) : base(world) { }
+
+        public override void Initialize()
+        {
+            Debug.Log("DebugCreateObjectSystem Initialized!");
+            
+        }
+
+        public override void Update(in float deltaTime)
+        {
+            if (Input.GetKeyDown(UnityEngine.KeyCode.A))
+            {
+                var entity = World.Create(new GameObjectComponent());
+                Debug.Log($"Created {entity}");
+            }
+
+
+            if (Input.GetKeyDown(UnityEngine.KeyCode.Q))
+            {
+                var query = new QueryDescription().WithAll<GameObjectComponent>();
+                World.Query(in query, (Entity entity,
+                    ref GameObjectComponent gameObjectComponent) =>
+                {
+                    World.Remove<GameObjectComponent>(entity);
+                    Debug.Log($"Remove {entity}");
+                });
+                
+            }
+        }
+    }
+}
