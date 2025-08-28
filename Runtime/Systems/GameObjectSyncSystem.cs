3ebbb56f0d8eca774cb4ee1c91b540fa23a73f2a
Bli_Aik <haikun2333@126.com>
2025-08-28 22:57:59 +0800

    feat(Unity): Add entity naming and refactor GameObjectSyncSystem
    Add NameComponent to debug entities and extend GameObjectSyncSystem to handle name synchronization. Introduce helper method for GameObject retrieval.

--- Runtime/Systems/GameObjectSyncSystem.cs
+++ Runtime/Systems/GameObjectSyncSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using Arch.Core;
 using Lunar.Components;
 using Lunar.Systems;
@@ -8,14 +9,29 @@ namespace Lunar.Adapters.Unity.Systems
     public class GameObjectSyncSystem : GameObjectSyncSystemBase
     {
         public GameObjectSyncSystem(World world) : base(world) { }
-        protected override void ApplyTransform(GameObjectComponent gameObject, PositionComponent position)
-        {
-            var unityGameObject = gameObject.GameObject.BaseGameObject as UnityEngine.GameObject;
 
-            if (unityGameObject)
+        protected override void SyncTransform(GameObjectComponent gameObject, PositionComponent position)
+        {
+            if (TryGetUnityGameObject(gameObject, out var unityGameObject))
             {
                 unityGameObject.transform.position = new Vector3(position.X, position.Y, position.Z);
             }
+        }
+
+        protected override void SyncName(GameObjectComponent gameObject, NameComponent name)
+        {
+            if (TryGetUnityGameObject(gameObject, out var unityGameObject))
+            {
+                unityGameObject.name = name.Name;
+            }
+        }
+
+        private static bool TryGetUnityGameObject(GameObjectComponent gameObject,
+            out UnityEngine.GameObject unityGameObject)
+        {
+            unityGameObject = gameObject.GameObject.BaseGameObject as UnityEngine.GameObject;
+
+            return unityGameObject;
         }
     }
 }
